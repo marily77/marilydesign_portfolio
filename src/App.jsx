89ccd1668c69c55da5632720c8d40ae46968e891
marily77 <marily77@gmail.com>
2026-07-11 22:13:19 +0300

    Add Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
